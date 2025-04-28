dc291fc4d9df31cd7603c6760ba532ae3f144de8
Erkin <erksapa@gmail.com>
2025-04-28 17:28:15 +0500

    ion

--- collect_files.sh
+++ collect_files.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $# -lt 2 ]]; then
-    echo "Usage: $0 <input_dir> <output_dir> [--max_depth N]"
-    exit 1
-fi
 
 input_dir="$1"
 output_dir="$2"
@@ -11,72 +7,53 @@ max_depth=0
 shift 2
 
 while [[ $# -gt 0 ]]; do
-    case "$1" in 
-            (--max_depth)
+    case "\$1" in
+        --max_depth)
             if [[ ! "$2" =~ ^[0-9]+$ ]]; then
+                echo "Error: --max_depth must be a positive integer"
                 exit 1
             fi
             max_depth="$2"
             shift 2
             ;;
-        
+        *)
+            echo "Unknown option: \$1"
+            exit 1
+            ;;
     esac
 done
 
-
 mkdir -p "$output_dir"
+declare -A file_counters
 
-find "$input_dir" -type f | while read -r file; do
-    rel_path="${file#$input_dir/}"
-    depth=$(( $(tr -cd '/' <<< "$rel_path" | wc -c) + 1 ))
-    
-    if [[ $max_depth -gt 0 && $depth -gt $max_depth ]]; then
-        continue
-    fi
-    
+find_args=(-type f)
+
+if [[ $max_depth -gt 0 ]]; then
+    find_args+=(-maxdepth "$max_depth")
+fi
+
+find "$input_dir" "${find_args[@]}" | while read -r file; do
     filename=$(basename "$file")
     dest="$output_dir/$filename"
-    counter=1
-    
-    while [[ -e "$dest" ]]; do
+
+    if [[ -e "$dest" ]]; then
+        if [[ -z "${file_counters[$filename]}" ]]; then
+            file_counters[$filename]=1
+        fi
+        counter=${file_counters[$filename]}
         name="${filename%.*}"
         ext="${filename##*.}"
-        
+
         if [[ "$name" != "$ext" ]]; then
             dest="$output_dir/${name}_$counter.$ext"
         else
             dest="$output_dir/${name}_$counter"
         fi
-        
-        ((counter++))
-    done
-    
+        ((file_counters[$filename]++))
+    fi
+
     cp "$file" "$dest"
 done
 
-echo "Copied all files to $output_dir"
+echo "Files copied successfully to $output_dir"
 
-
-    if [[ $max_depth -gt 0 && $depth -gt $max_depth ]]; then
-        continue
-    fi
-
-    base_name=$(basename "$src_file")
-    dest_file="$output_dir/$base_name"
-    counter=1
-
-    while [[ -e "$dest_file" ]]; do
-        name_part="${base_name%.*}"
-        ext_part="${base_name##*.}"
-        
-        if [[ "$name_part" != "$ext_part" ]]; then
-            dest_file="$output_dir/${name_part}_$counter.$ext_part"
-        else
-            dest_file="$output_dir/${base_name}_$counter"
-        fi
-        
-        ((counter++))
-    done
-
-    cp "$src_file" "$dest_file"
-done
