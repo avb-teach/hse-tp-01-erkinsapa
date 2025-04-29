ef552060c7fb62f14877f4e3f207d88f84538521
Erkin <erksapa@gmail.com>
2025-04-29 11:39:06 +0500

    rik

--- collect_files.sh
+++ collect_files.sh
@@ -3,28 +3,30 @@
 input_dir="$1"
 output_dir="$2"
 max_depth=-1
-
 shift 2
 
 while [ $# -gt 0 ]; do
     if [ "$1" = "--max_depth" ]; then
+        if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+            exit 1
+        fi
         max_depth="\$2"
         shift 2
-    else"
+    else
         exit 1
     fi
 done
 
 mkdir -p "$output_dir"
 
-declare -A file_counters
+declare -A counters
 
-findargs=(-type f)
+args=(-type f)
 if [ $max_depth -ge 0 ]; then
-    findargs+=(-maxdepth "$max_depth")
+    args+=(-maxdepth "$max_depth")
 fi
 
-find "$input_dir" "${find_args[@]}" | while IFS= read -r file; do
+find "$input_dir" "${args[@]}" | while IFS= read -r file; do
     relative_path="${file#$input_dir/}"
     destination="$output_dir/$relative_path"
 
@@ -34,27 +36,26 @@ find "$input_dir" "${find_args[@]}" | while IFS= read -r file; do
         base="${filename%.*}"
         extension="${filename##*.}"
 
-        if [ "$base_name" = "$extension" ]; then
+        if [ "$base" = "$extension" ]; then
             base="${filename}"
             extension=""
         fi
 
-        if [ -z ${file_counters["$filename"]} ]; then
-            file_counters["$filename"]=1
+        if [ -z ${counters["$filename"]} ]; then
+            counters["$filename"]=1
         else
-            file_counters["$filename"]=$((file_counters["$filename"] + 1))
+            counters["$filename"]=$((counters["$filename"] + 1))
         fi
 
-        suffix=${file_counters["$filename"]}
+        suffix=${counters["$filename"]}
         if [ -n "$extension" ]; then
-            destination="$dir/${base_name}_$suffix.$extension"
+            destination="$dir/${base}_$suffix.$extension"
         else
-            destination="$dir/${base_name}_$suffix"
+            destination="$dir/${base}_$suffix"
         fi
     fi
 
     mkdir -p "$(dirname "$destination")"
     cp "$file" "$destination"
 done
-
 
